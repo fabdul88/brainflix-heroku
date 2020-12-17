ebebdad8c7a704cec6703ff2a786e5d13792b928
abdul farhan <kukluu@hotmail.com>
2020-12-16 18:18:21 -0800

    build folder created

---
+++ client/build/precache-manifest.7abe144942112a8f062ff978d525ef00.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94a61dd0c8cba7ca0c61281438f2989b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4390f2f8811e009b9dc5",
+    "url": "/static/css/main.aad09879.chunk.css"
+  },
+  {
+    "revision": "87733c3d63006d64d147",
+    "url": "/static/js/2.07bd4961.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.07bd4961.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4390f2f8811e009b9dc5",
+    "url": "/static/js/main.86ce9f65.chunk.js"
+  },
+  {
+    "revision": "859b613a0c0f63971834",
+    "url": "/static/js/runtime-main.62d41f92.js"
+  },
+  {
+    "revision": "03b74b4c4dc7cd48eee599a4517f1441",
+    "url": "/static/media/Icon-fullscreen.03b74b4c.svg"
+  },
+  {
+    "revision": "23c3f8579420d52ffd5c13ad742baa6a",
+    "url": "/static/media/Icon-likes.23c3f857.svg"
+  },
+  {
+    "revision": "a3c51b76d2b01afe928cef3c6b522f21",
+    "url": "/static/media/Icon-play.a3c51b76.svg"
+  },
+  {
+    "revision": "286069001111ea3ed233483011ce0c09",
+    "url": "/static/media/Icon-search.28606900.svg"
+  },
+  {
+    "revision": "909b8976a003b06080b276c94fcfe418",
+    "url": "/static/media/Icon-upload.909b8976.svg"
+  },
+  {
+    "revision": "a488fc5b5ba8a1b910c6b8278131ffac",
+    "url": "/static/media/Icon-views.a488fc5b.svg"
+  },
+  {
+    "revision": "d17c2fe7a1d599b46d1ce25f73786da2",
+    "url": "/static/media/Icon-volume.d17c2fe7.svg"
+  },
+  {
+    "revision": "222586f42fa280e476462914cdf70dec",
+    "url": "/static/media/Linotype-AvenirNextLTPro-Bold.222586f4.otf"
+  },
+  {
+    "revision": "1a1c2c769bb1a94529f507e96d6985ac",
+    "url": "/static/media/Linotype-AvenirNextLTPro-Demi.1a1c2c76.otf"
+  },
+  {
+    "revision": "67016125d8abf229098a0195c7f9ed64",
+    "url": "/static/media/Linotype-AvenirNextLTPro-Regular.67016125.otf"
+  },
+  {
+    "revision": "1e121e6bb74d3a6c39ed045da009ca40",
+    "url": "/static/media/Logo-brainflix.1e121e6b.svg"
+  },
+  {
+    "revision": "ba2f48f9885e020bc9bf2c8c442af56a",
+    "url": "/static/media/Mohan-muruge.ba2f48f9.jpg"
+  },
+  {
+    "revision": "c814c81c34ab0a6574dffc33fcec7fa1",
+    "url": "/static/media/Upload-video-preview.c814c81c.jpg"
+  }
+]);
