20c2bb6d233105f70abd0ae57adb665cefdfd0f3
abdul farhan <kukluu@hotmail.com>
2021-01-07 21:49:53 -0800

    deploy test

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ app.post("/videos", (req, res) => {
 
 // Setting up for deployment
 if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
   const path = require("path");
-  // app.use(express.static(path.resolve(__dirname, "client", "build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
